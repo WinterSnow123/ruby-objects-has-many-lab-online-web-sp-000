6e4d0b956db684992204ac248384e90e4317d9bd
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-29 02:47:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,18 +5,15 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
     song.artist = self
-    @songs << song
   end
 
   def add_song_by_name(song_name)
     song_name = Song.new(song_name)
     song_name.artist = self
-    @songs << song_name
   end
 
   def songs
