f697d9f629951b4f733596c77ee9f2bf1e8a45ae
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-29 02:27:04 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    song_name.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end
